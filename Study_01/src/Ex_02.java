4e7aeadc8785fc65b0b4387d7cd8b55159ed3463
smhrd <smhrd@DESKTOP-LUL7B9D>
2022-06-16 12:35:22 +0900

    변수 선언과 연산자를 이용한 연산

---
+++ Study_01/src/Ex_02.java
@@ -0,0 +1,19 @@
+
+public class Ex_02 {
+
+	public static void main(String[] args) {
+
+		// 변수 선언
+		int a = 30;
+		int b = 10;
+		
+		// 연산자 (+, -, *, /, %)
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+	}
+
+}
